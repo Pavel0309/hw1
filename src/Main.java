b212607973578fca02d74862a55ea649b9507b4c
homework4 <doroshev.p@yandex.ru>
2023-03-08 00:26:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,202 @@
+package org.future.code.homework;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * Правила выполнения домашнего задания:
+ * <p>
+ * 1. Вся работа выполняется в файле доамашнего задания
+ * Если вас просят создать класс - его надо создать вложенным, т.е. внутри класса HomeWorkN
+ * вы создаете класс MyClass {} (все в одном файле)
+ * 2. Задание считается выполненным если все тесты пройдены.
+ * Тест выполнен если напротив него находится знак ✅
+ * 3. Что бы запустить тесты нажмите значек запуска кода напротив мтода main.
+ * ‼️ВАЖНО‼️ Вся работа в модкле ведется на JDK-17, перед запуском скачайте его и установите
+ * правильный JDK и уровень языка в Settings -> Project Structure
+ */
+public class HomeWork1 {
+    /*
+    Домашнее задание #1:
+
+    У всех полей выставить модфикатор доступа private
+
+    Реализовать класс «Student»
+        Поля класса - name:String, grade:Integer (+конструктор, +геттеры)
+        Метод announce - String announce() {} - возвращает строку в формате "/name/ учится в /grade/ классе"
+
+    Реализовать класс «Teacher»
+        Поля класса - name:String, students:Student[30] (+конструктор из name, +геттеры)
+        Метод - void addStudent(Student student) {} - добавляет студента в массив студентов,
+            если колличество студентов достигло максимального колличества, ничего не делать
+        Метод - String[] rollCall() {} - возвращает массив строк из результатов вызова метода
+            announce всех встудентов преподавателя
+     */
+    public static class Student {
+
+        int x = 0;
+
+        int age;
+        private int grade;
+        private String name;
+        static int population = 0;
+
+        Student(int newAge, int newGrade, String newName) {
+            age = newAge;
+            grade = newGrade;
+            name = newName;
+        }
+
+        void setage(int newAge) {
+            if (newAge > 18 && newAge < 150) {
+                age = newAge;
+            } else {
+                System.out.println("incorrect age");
+            }
+        }
+
+        int Anounce() {
+            x++;
+            System.out.println(name + " grade " + grade + " age " + age);
+            return x;
+        }
+
+        Student(int inputAge, String inputName) { // Конструктор с параметрами
+            age = inputAge;
+            name = inputName;
+            population++;
+            System.out.println("Human age&name Constructor");
+        }
+
+        static void newBorn() { // Статический метод может использовать только
+            population++;       // статические переменные и не может использовать
+        }                       // ключевое слово this
+
+        Student(Student h) { // Копирующий конструктор
+            age = h.age;
+            name = h.name;
+            population++;
+            System.out.println("student copy Constructor");
+        }
+
+        void setname(String newName) {
+            name = newName;
+        }
+
+        String getName() {
+            return name;
+        }
+
+        int getage() {
+            return age;
+        }
+
+        void setGrade(int newGrade) {
+            if (newGrade > 1 && newGrade < 4) {
+                grade = newGrade;
+            } else {
+                System.out.println("incorrect grade");
+            }
+        }
+
+        int getGrade() {
+            return grade;
+        }
+
+
+        // Напиши здесь свою реализацию класса Student
+    }
+
+    public static class Teacher {
+        String name;
+        int population;
+
+        Teacher(String name) {
+            population = 0;
+            this.name = name;
+        }
+
+
+        Student[] students = new Student[30];
+
+        void setName(String name) { // Прописываем сеттер для имени
+            this.name = name;
+        }
+
+        String getName() { // Прописываем геттер для имени
+            return this.name;
+        }
+
+
+        void addStudent(Student student) {
+
+           // students[population] = student;
+           // population++;
+            if(population<=30){
+                students[population] = student;
+                population++;
+            }
+
+        }
+
+        String[] rollcall = {Student.Anounce("ll", 33)};
+
+        // First method
+        //  Student[] students = new Student[30];
+        //  for {
+        //      System.out.print(i + " ");
+    }
+            System.out.println();
+    // Напиши здесь свою реализацию класса Teacher
+}
+
+    /*
+    Это метод main - нажми play что бы запустить тесты
+    Ничего не меняй в тестах, они уже написаны так что бы проверить твое решение
+    Некторые методы в тесте подсвечены красным, это значит что компилятор не может их найти
+    и тебе необходимо их релизовать, пока ты это не сделаешь, тесты не запустятся
+    */
+    public static void main(String[] args) {
+
+        var student = new Student(STUDENT_NAME, STUDENT_GRADE);
+        print("Student: Студент создался", true);
+        print("Student: Геттер имени", Objects.equals(student.getName(), STUDENT_NAME));
+        print("Student: Геттер класса", Objects.equals(student.getGrade(), STUDENT_GRADE));
+        print("Student: announce содержит имя", student.announce().contains(STUDENT_NAME));
+        print("Student: announce содержит класс", student.announce().contains(STUDENT_GRADE.toString()));
+
+        var teacher = new Teacher(TEACHER_NAME);
+        print("Teacher: Уичтель создался", true);
+        print("Teacher: Геттер имени", teacher.getName() == TEACHER_NAME);
+        print("Teacher: Геттер студентов", teacher.getStudents() != null);
+        print("Teacher: Массив учеников должен быть размером 30", teacher.getStudents().length == 30);
+
+        teacher.addStudent(student);
+        print("Teacher: Студент сохранился в массив", teacher.getStudents()[0] == student);
+        String[] calls = teacher.rollCall();
+        print("Teacher: Массив rollCall состоит из одиного элемента", calls.length == 1);
+        print("Teacher: В строке содержится имя студента", calls[0].contains(STUDENT_NAME));
+    }
+
+    /* Техническая секция - сюда писать ничего не надо */
+
+    private static void print(String condition, Boolean act) {
+        Function<String, String> yellow = str -> "\u001B[33m" + str + "\u001B[0m";
+        System.out.print("TEST CASE " + yellow.apply(constLen(condition, 55)));
+        if (act) System.out.print("✅");
+        else System.out.print("❌");
+        System.out.println();
+    }
+
+    private static String constLen(String str, int len) {
+        StringBuilder sb = new StringBuilder(str);
+        while (len-- - str.length() > 0) sb.append(" ");
+        return sb.toString();
+    }
+
+    private final static String STUDENT_NAME = "NameStudent";
+    private final static String TEACHER_NAME = "NameStudent";
+    private final static Integer STUDENT_GRADE = 1;
+}
